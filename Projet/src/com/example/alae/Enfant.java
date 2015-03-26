de697e7dc6a1869fae2dbb67aeca66b92637a7f7
QJF <denisq@saintemarie-cholet.eu>
2015-03-26 08:08:22 +0100

    Mecanique interne: enfant / famille / parent

---
+++ Projet/src/com/example/alae/Enfant.java
@@ -0,0 +1,17 @@
+package com.example.alae;
+import java.util.*;
+
+public class Enfant 
+{
+	private String nom;
+	private String prenom;
+	private Date dateNaissance;
+	
+	public Enfant(String nom, String prenom, Date dateNaissance) 
+	{
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+	}
+	
+}
